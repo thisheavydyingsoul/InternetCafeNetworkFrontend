a6a9e55a5fa45cb025e63ebefab0a8487e5f945a
Kirill <izranovks@gmail.com>
2026-09-09 18:44:33 +0800

    fix: app config unused imports removed in both projects

--- projects/admin/src/app/app.config.ts
+++ projects/admin/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
