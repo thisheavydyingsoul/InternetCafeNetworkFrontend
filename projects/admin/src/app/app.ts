f161b4f6fe838171d572919409dcad47e6f809dc
Kirill <izranovks@gmail.com>
2026-09-09 18:57:30 +0800

    fix: app ts in admin project prefix changed to admin from app

--- projects/admin/src/app/app.ts
+++ projects/admin/src/app/app.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   imports: [RouterOutlet],
-  selector: 'app-root',
+  selector: 'admin-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
